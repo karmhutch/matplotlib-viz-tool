52e9eb66011717c294294b6e64b3cc798a680dcf
Karmen Hutchinson <karmen.hutchinson@gmail.com>
2023-12-25 23:16:00 -0500

    first pass at populating setup files [MVT-30]

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 25 14:19:21 2023
 
-@author: karmen
-"""
+from setuptools import setup, find_packages
+
+setup(
+      name="vizkarmhutch",
+      version="0.0.0",
+      description="A visualization tool using Matplotlib",
+      author="Karmen Hutchinson",
+      author_email="karmen.hutchinson@gmail.com",
+      url="https://github.com/karmhutch/matplotlib-viz-tool",
+      packages=find_packages()
+)
