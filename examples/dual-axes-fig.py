c2272e771687ce50834285bdc5d92de596b3d5e3
Karmen Hutchinson <karmen.hutchinson@gmail.com>
2024-01-05 13:16:23 -0500

    remove files [MVT-22]

--- examples/dual-axes-fig.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 25 17:04:29 2023
-
-@author: karmen
-"""
--- examples/quad-axes-fig.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 25 17:10:29 2023
-
-@author: karmen
-"""
